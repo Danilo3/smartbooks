2397e22b390b8f71bd154e30040c29ed37ae0c7c
Danilo3 <danil.2010846@yandex.ru>
2018-01-31 15:19:22 +0300

    udalil govnokod

--- api-gateway/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO login_user (id, username, password, role) VALUES (1, 'Alex123', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 'ROLE_ADMIN');
-INSERT INTO login_user (id, username, password,role) VALUES (2, 'Tom234', '$2a$04$PCIX2hYrve38M7eOcqAbCO9UqjYg7gfFNpKsinAxh99nms9e.8HwK','ROLE_USER');
-INSERT INTO login_user (id, username, password, role) VALUES (3, 'Adam', '$2a$04$I9Q2sDc4QGGg5WNTLmsz0.fvGv3OjoZyj81PrSFyGOqMphqfS2qKu', 'ROLE_USER');
--- util/src/main/java/com/koryakin/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.koryakin.util;
-
-public class Constants {
-
-     static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
-     static final String SIGNING_KEY = "keyKor7";
-     public static final String TOKEN_PREFIX = "Bearer ";
-     public static final String HEADER_STRING = "Authorization";
-}
