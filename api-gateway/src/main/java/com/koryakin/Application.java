6ab4855cf3279917cec355682f1859991c1c63f4
Danilo3 <danil.2010846@yandex.ru>
2018-02-05 16:48:25 +0300

    soe commit

--- api-gateway/src/main/java/com/koryakin/Application.java
+++ api-gateway/src/main/java/com/koryakin/Application.java
@@ -1,15 +1,22 @@
 package com.koryakin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableZuulProxy
+//@EnableZuulProxy
 @SpringBootApplication
 public class Application extends WebMvcConfigurerAdapter{
 
+
+    @SuppressWarnings("unused")
+    private static final transient Logger LOG = LoggerFactory.getLogger(Application.class.getName());
+
     public static void main(String[] args) {
+        LOG.info("Application started");
         SpringApplication.run(Application.class, args);
     }
 
---
+++ api-gateway/src/main/java/com/koryakin/controllers/UserAuthController.java
@@ -0,0 +1,4 @@
+package com.koryakin.controllers;
+
+public class UserAuthController {
+}
